dcf831cacfe10be829b9d9187b52fa281bff4de4
ashwinsingh <ashwinjsingh012@gmail.com>
2023-12-03 12:45:55 -0800

    qwer

--- _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
+++ _01_elif/_2_crazy_cat_lady/crazy_cat_lady.py
@@ -10,10 +10,17 @@ def play_video(url):
 
 
 if __name__ == '__main__':
-    # TODO 1) Make a new window variable, window = Tk()
-    #      2) Hide the window using the window's .withdraw() method
-    #      3) Ask the user how many cats they have
-    #      4) If they have 3 or more cats, tell them they are a crazy cat lady
+    window = Tk()
+    window.withdraw()
+    number = simpledialog.askstring(title="hi",prompt="how many cats do you have?")
+    number = int(number)
+    if number >= 3:
+        messagebox.showinfo(title="hi",message="You're a crazy cat person")
+    if number >0 and number <3:
+        messagebox.showinfo(title="hi",message="hmm,ok")
+    if number == 0:
+        messagebox.showinfo(title="hi",message="Yay, no cats!")
+
     #      5) If they have less than 3 cats AND more than 0 cats, call the
     #         play_video function below to show them a cat video
     #      6) If they have 0 cats, show them a video of A Frog Sitting on a
--- _01_elif/_3_are_you_happy/are_you_happy.py
+++ _01_elif/_3_are_you_happy/are_you_happy.py
@@ -4,5 +4,16 @@ from tkinter import Tk, simpledialog, messagebox
 if __name__ == '__main__':
     # TODO: Look at the AreYouHappy.png image
     #       Use pop-ups to recreate the chart using if and elif statements
+    window = Tk()
+    window.withdraw()
+    happy = simpledialog.askstring(title='hi', prompt= 'are you happy? Yes or No only')
+    if happy == "Yes":
+        messagebox.showinfo(title='hi',message='keep doing whatever you are doing')
+    if happy == "No":
+        happy_two = simpledialog.askstring(title="hi", prompt='Do you want to be happy?')
+        if happy_two == "No":
+            messagebox.showinfo(title="hi", message="keep doing what ever you are doing")
+        if happy_two == "Yes":
+            messagebox.showinfo(title="hi",message="Change something")
 
     pass
